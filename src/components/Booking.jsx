4b4eb43e3ac2c19dc80ec8801b39d5b819c2e875
martins0023 <martinsmiracle45@gmail.com>
2025-01-15 23:16:49 +0100

    booking page

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,5 +1,4 @@
 import { CheckCircle2 } from "lucide-react";
-import { pricingOptions } from "../constants";
 import { motion } from "framer-motion";
 import { staggerContainer, textVariants } from "../constants/animations";
 import { checklistItems } from "../constants";
@@ -21,26 +20,44 @@ const Booking = () => {
       >
         Appointment Booking
       </motion.h2>
-      
+
       <div className="flex flex-wrap justify-center">
-        <div className="p-2 w-full lg:w-1/2 h-fit">
-          <img src={calendarmockup} alt="Appointment Booking" className="mt-5" />
-        </div>
         <div className="pt-12 w-full lg:w-1/2">
-          
-            <div className="flex mb-12">
-              {/* <div className="text-purple-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
-                <CheckCircle2 />
-              </div> */}
-              <div>
-                <p className="text-md text-neutral-500">Booking with Doctor Kays is easy and convenient. You can book an appointment through the booking section of their website by selecting any service. You can either have a virtual consultation for convenience or meet him in person if you want it more personal; both options are available. The platform offers an easy-to-use interface that ensures a smooth booking process. You can see available time slots and book an appointment immediately after selecting the service you want. Doctor Kays's online booking system puts accessibility and efficiency at the core for all users.</p>
+          <div className="flex mb-12">
+            <div className="md:pt-5">
+              <p className="text-md tracking-wider gap-auto text-neutral-400">
+                Booking with Doctor Kays is easy and convenient. You can book an
+                appointment through the booking section of their website by
+                selecting any service. You can either have a virtual
+                consultation for convenience or meet him in person if you want
+                it more personal; both options are available. The platform
+                offers an easy-to-use interface that ensures a smooth booking
+                process. You can see available time slots and book an
+                appointment immediately after selecting the service you want.
+                Doctor Kays's online booking system puts accessibility and
+                efficiency at the core for all users.
+              </p>
+
+              <div className="flex justify-center">
+                <Button
+                  text="Book an Appointement"
+                  className="mt-5 hover:bg-white  rounded-tl-full rounded-bl-full py-2 px-3 bg-gradient-to-r from-purple-500 to-purple-950"
+                />
               </div>
             </div>
-          
+          </div>
+        </div>
+        <div className="p-2 w-full lg:w-1/2 h-fit">
+          <motion.img
+            variants={slideInFromRight}
+            src={calendarmockup}
+            alt="calendar-mockup"
+            className="rounded-xl"
+          />
         </div>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Booking
+export default Booking;
