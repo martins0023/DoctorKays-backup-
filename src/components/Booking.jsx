938e675892cdf35c0d401b9e45d80c1e878e9fb4
martins0023 <martinsmiracle45@gmail.com>
2025-01-15 22:32:27 +0100

    worked on new pages

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -28,7 +28,7 @@ const Booking = () => {
         </div>
         <div className="pt-12 w-full lg:w-1/2">
           
-            <div key={index} className="flex mb-12">
+            <div className="flex mb-12">
               {/* <div className="text-purple-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                 <CheckCircle2 />
               </div> */}
