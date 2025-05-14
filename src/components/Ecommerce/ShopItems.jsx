ca64d6150d09de1405d2f4aa6a504f7c0301a3f6
cluster_wraith <69491293+martins0023@users.noreply.github.com>
2025-05-14 02:58:19 +0100

    Update ShopItems.jsx

--- src/components/Ecommerce/ShopItems.jsx
+++ src/components/Ecommerce/ShopItems.jsx
@@ -22,7 +22,7 @@ const ShopItems = ({ limit, horizontal }) => {
 
   const navigate = useNavigate();
   const handleProductClick = (product) => {
-    navigate(`/product/${product.id}`, { state: { product } });
+    navigate(`/product/${product.id}`;
     window.scrollTo(0, 0);
   };
 
